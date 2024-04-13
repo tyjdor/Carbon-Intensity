f4adf0f0f7bc8340202dd81c13ad6b893ffd813f
tyjdor <tyjdor96@gmail.com>
2024-04-13 15:12:28 -0400

    convert app to tsx

---
+++ carbon-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CarbonIntensity from "./CarbonIntensity";
+
+function App() {
+  return (
+    <>
+      <CarbonIntensity />
+    </>
+  );
+}
+
+export default App;
