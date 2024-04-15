b948312d33f67df89b949d1e78a6b6ea8340de3a
tyjdor <tyjdor96@gmail.com>
2024-04-15 17:35:31 -0400

    add router to app

--- carbon-app/src/App.tsx
+++ carbon-app/src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CarbonIntensity from "./CarbonIntensity";
+import RegionDetails from "./RegionDetails"; // Import your RegionDetails component
+import { Context } from "./Context";
 
-function App() {
+const App = () => {
+  const [region, setRegion] = useState();
   return (
-    <>
-      <CarbonIntensity />
-    </>
+    <Context.Provider value={{ region, setRegion }}>
+      <Routes>
+        <Route path="/" element={<CarbonIntensity />} />
+        <Route path="/region/:id" element={<RegionDetails />} />
+      </Routes>
+    </Context.Provider>
   );
-}
+};
 
 export default App;
--- carbon-app/src/index.tsx
+++ carbon-app/src/index.tsx
@@ -1,6 +1,13 @@
-import React from "react";
-import { render } from "react-dom";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-render(App(), document.getElementById("root"));
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
