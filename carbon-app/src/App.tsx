eab6a2f10addae8a6abd0aa3ca6ea40e93fab83f
tyjdor <tyjdor96@gmail.com>
2024-04-16 12:49:10 -0400

    update context for more values

--- carbon-app/src/App.tsx
+++ carbon-app/src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CarbonIntensity from "./CarbonIntensity";
-import RegionDetails from "./RegionDetails"; // Import your RegionDetails component
+import RegionDetails from "./RegionDetails";
 import { Context } from "./Context";
 
 const App = () => {
   const [region, setRegion] = useState();
+  const [regionalData, setRegionalData] = useState();
   return (
-    <Context.Provider value={{ region, setRegion }}>
+    <Context.Provider
+      value={{ region, setRegion, setRegionalData, regionalData }}
+    >
       <Routes>
         <Route path="/" element={<CarbonIntensity />} />
         <Route path="/region/:id" element={<RegionDetails />} />
